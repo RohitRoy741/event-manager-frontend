d6366af58b3d85e9135b032bd2648f6893bff409
Salamander <rohitroy741760@gmail.com>
2021-07-15 16:57:04 +0530

    Added Icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -25,11 +25,21 @@ function Header(props){
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className="navbar-items">
                 <Nav>
-                <Nav.Link href="#home" className="navbar-item username">{user.username}</Nav.Link>
+                <Nav.Link href="#home" className="navbar-item username">
+                    {user.username}
+                </Nav.Link>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 user-icon web" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
                 <Nav.Link href="#link"  className="navbar-item" onClick={()=>handleShow(true)}>Create-Event</Nav.Link>
                 <Nav.Link href="#schedule" className="navbar-item">Schedule</Nav.Link>
                 <Nav.Link href="#my-events" className="navbar-item">My Events</Nav.Link>
-                <Nav.Link href="#history" className="navbar-item" onClick={props.handleLogOut}>Logout</Nav.Link>
+                <Nav.Link href="#history" className="navbar-item logout" onClick={props.handleLogOut}>Logout</Nav.Link>
+                <div onClick={props.handleLogOut}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 logout-icon web" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                    </svg>
+                </div>
                 </Nav>
             </Navbar.Collapse>
             </Navbar>
